439f5245f2341ff70e833e41d3a3cba1b079596c
Mikel Larreategi <mlarreategi@codesyntax.com>
2012-07-10 10:21:48 +0200

    add translations from plonetruegallery

--- collective/ptg/contactsheet/__init__.py
+++ collective/ptg/contactsheet/__init__.py
@@ -1,11 +1,12 @@
 from collective.plonetruegallery.utils import createSettingsFactory
 from zope.schema.vocabulary import SimpleVocabulary, SimpleTerm
-from collective.plonetruegallery import PTGMessageFactory as _
 from collective.plonetruegallery.browser.views.display import \
     BatchingDisplayType
 from collective.plonetruegallery.interfaces import IBaseSettings
 from zope import schema
+from zope.i18nmessageid import MessageFactory
 
+_ = MessageFactory('collective.ptg.contactsheet')
 
 class IContactsheetDisplaySettings(IBaseSettings):
     contactsheet_columns = schema.Int(
